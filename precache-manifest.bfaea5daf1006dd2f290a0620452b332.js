4ca1c06ffa89534cff6fa2058849103f34c88a74
shrvr <shurvir04@gmail.com>
2020-12-21 18:44:15 +0530

    Updates

---
+++ precache-manifest.bfaea5daf1006dd2f290a0620452b332.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d010d88e22bed8a85b32343552a485",
+    "url": "/placement-portal/index.html"
+  },
+  {
+    "revision": "4f0af91bb1b756738970",
+    "url": "/placement-portal/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "1d08dcffbd64a9040b52",
+    "url": "/placement-portal/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "4f0af91bb1b756738970",
+    "url": "/placement-portal/static/js/2.b443f9ca.chunk.js"
+  },
+  {
+    "revision": "45e57682b05434ca5bd450bf10eb18e7",
+    "url": "/placement-portal/static/js/2.b443f9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d08dcffbd64a9040b52",
+    "url": "/placement-portal/static/js/main.aa685f18.chunk.js"
+  },
+  {
+    "revision": "685c57c06b3960f8ff6d",
+    "url": "/placement-portal/static/js/runtime-main.f6e6181a.js"
+  },
+  {
+    "revision": "39fe70843c9820aec03ed5afefec0de9",
+    "url": "/placement-portal/static/media/capgemini_logo.39fe7084.png"
+  },
+  {
+    "revision": "a972e5a088515d3de9afdbdb4e8ffd3f",
+    "url": "/placement-portal/static/media/ldrp_logo.a972e5a0.png"
+  }
+]);
